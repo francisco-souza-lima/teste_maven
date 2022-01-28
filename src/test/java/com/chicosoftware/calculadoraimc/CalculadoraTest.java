898c982ffc786b8e90afd6b6ce1d1f056dd5e6f6
Michel Jader <michel.jader@luizalabs.com>
2022-01-28 14:37:01 -0300

    melhoria no teste

--- src/test/java/com/chicosoftware/calculadoraimc/CalculadoraTest.java
+++ src/test/java/com/chicosoftware/calculadoraimc/CalculadoraTest.java
@@ -33,15 +33,19 @@ public class CalculadoraTest {
     public void calcular_qual_mais_saudavel() {
         Calculadora c = new Calculadora();
         List<Pessoa> pessoas = new ArrayList<>();
+
+        Pessoa p0 = new Pessoa("Fino", 10, 1);
         Pessoa p1 = new Pessoa("Zeca", 1.90, 45);
         Pessoa p2 = new Pessoa("Francisco", 1.70, 66);
         Pessoa p3 = new Pessoa("Maicon", 2.00, 88);
         Pessoa p4 = new Pessoa("Jefferson", 1.60, 77);
+
+        pessoas.add(p0);
         pessoas.add(p3);
         pessoas.add(p4);
         pessoas.add(p1);
         pessoas.add(p2);
         String pessoaMaisSaudavel = c.quemTemOMenorIMC(pessoas);
-        Assert.assertEquals("Zeca",pessoaMaisSaudavel);
+        Assert.assertEquals("Fino",pessoaMaisSaudavel);
     }
 }
